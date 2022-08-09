9c4e841d66e97a8622a5474dd55e355eb976b3d9
admin <admin@YD-11>
2022-08-09 10:11:41 +0900

    0809 lombok

--- CoffeeExam/src/co/cofe/Coffee.java
+++ CoffeeExam/src/co/cofe/Coffee.java
@@ -1,42 +1,16 @@
 package co.cofe;
 
 import co.coffee.common.DAO;
+import lombok.Data;
+import lombok.ToString;
+//@NoArgsConstructor //매개변수가 없는 기본 생성자 만들어줌
+//@AllArgsConstructor //모든 필드를 가져와서 만드는 생성자
+@ToString //오버라이딩과 비슷
+@Data // 앞의 모든 명령어를 한번에 다 넣어줌
 
 public class Coffee extends DAO {
 	private String coffeeMenu;
 	private int coffeePrice;
 	private String coffeeExplain;
 	private int coffeeSales;
-
-	public String getCoffeeMenu() {
-		return coffeeMenu;
-	}
-
-	public void setCoffeeMenu(String coffeeMenu) {
-		this.coffeeMenu = coffeeMenu;
-	}
-
-	public int getCoffeePrice() {
-		return coffeePrice;
-	}
-
-	public void setCoffeePrice(int coffeePrice) {
-		this.coffeePrice = coffeePrice;
-	}
-
-	public String getCoffeeExplain() {
-		return coffeeExplain;
-	}
-
-	public void setCoffeeExplain(String coffeeExplain) {
-		this.coffeeExplain = coffeeExplain;
-	}
-
-	public int getCoffeeSales() {
-		return coffeeSales;
-	}
-
-	public void setCoffeeSales(int coffeeSales) {
-		this.coffeeSales = coffeeSales;
-	}
 }
